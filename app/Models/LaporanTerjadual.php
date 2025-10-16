1b2871ccc0c84fab04ec7b0105bc7be152ef9501
IzzatFirdaus <2021601074@student.uitm.edu.my>
2025-10-16 11:46:28 +0800

    fix: covariant PHPDoc type for LaporanTerjadual::fillable property for Larastan compliance

--- app/Models/LaporanTerjadual.php
+++ app/Models/LaporanTerjadual.php
@@ -51,7 +51,7 @@ class LaporanTerjadual extends Model
     /**
      * The attributes that are mass assignable.
      *
-     * @var array<int, string>
+     * @var list<string>
      */
     protected $fillable = [
         'nama',
